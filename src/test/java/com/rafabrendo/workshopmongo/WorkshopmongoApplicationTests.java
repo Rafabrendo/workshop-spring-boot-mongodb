35d1a245642263d4339bb427744c82639314e5c3
Rafael Brendo <rafalive122@hotmail.com>
2023-01-23 11:52:48 -0300

    Initial commit

---
+++ src/test/java/com/rafabrendo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafabrendo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
