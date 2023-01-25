afab3448f7b873c6e2e6a2ea543d669dd3c710df
Rafael Brendo <rafalive122@hotmail.com>
2023-01-25 14:44:03 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/rafabrendo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rafabrendo/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rafabrendo.workshopmongo.domain.Post;
 import com.rafabrendo.workshopmongo.domain.User;
 import com.rafabrendo.workshopmongo.dto.UserDTO;
 import com.rafabrendo.workshopmongo.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
