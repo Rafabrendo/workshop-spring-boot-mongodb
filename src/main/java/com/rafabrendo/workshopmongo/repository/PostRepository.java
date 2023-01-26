dc403ec9e449630539149d459e23be5f6784c98f
Rafael Brendo <rafalive122@hotmail.com>
2023-01-26 08:42:33 -0300

    Consulta simples com @Query

--- src/main/java/com/rafabrendo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rafabrendo/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.rafabrendo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafabrendo.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	//title -> campo pelo qual quero efetuar a busca //?0 (expressão regular) indica o parâmetro text //i -> Case insensitivity 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
