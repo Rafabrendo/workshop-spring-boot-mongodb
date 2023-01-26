38e5a1683224957d9ed48be71af02ee111ff5d94
Rafael Brendo <rafalive122@hotmail.com>
2023-01-26 09:35:20 -0300

    Consulta com vários critérios

--- src/main/java/com/rafabrendo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rafabrendo/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.rafabrendo.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,5 +16,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//?1 -> se refere ao minDate
+	@Query("{ $and: [ {date: { $gte: ?1 } } , {date: { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/rafabrendo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/rafabrendo/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.rafabrendo.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
 		text = URL.decodeParam(text);//decodifica o texto
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L)); //vai instanciar uma data minima, caso ocorra erro.
+		Date max = URL.convertDate(maxDate, new Date());//vai gerar uma data com o instante atual do sistema, caso ocorra erro.		
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 	
